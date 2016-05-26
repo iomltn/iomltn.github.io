4429710790faeab6a863c2142ee12aa8c1d67103
iomltn <iomltn@gmail.com>
2016-05-26 11:24:45 -0300

    corrigir popup

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,5 @@
 var estilo_popup = document.createElement("style");
-estilo_popup.innerHTML = "#popup {z-index: 1000; position: fixed; width: 550px; height: 396px; background: rgb(0, 153, 51); border-radius: 10px; padding: 2px; display: none; }";
+estilo_popup.innerHTML = "#popup {z-index: 1000; position: fixed; width: 550px; height: 396px; background: rgb(0, 153, 51); border-radius: 10px; padding: 2px; display: none; top: 50px; left: 50%; margin-left: -275px}";
 estilo_popup.innerHTML += "#popup #imagem {border-radius: 8px; display: none;}";
 estilo_popup.innerHTML += "#popup #imagem_fechar {position: absolute; top: -9px; right: -9px; cursor: pointer;}";
 document.getElementsByTagName("head")[0].appendChild(estilo_popup);
@@ -8,7 +8,7 @@ var top, left = 0;
 var clientWidth = document.body.clientWidth;
 var clientHeight = document.body.clientHeight;
 var scrollTop = 0;
-var visiblidade_popup = false;
+var visibilidade_popup = true;
 var popup = document.createElement("div");
 popup.setAttribute("id", "popup");
 var imagem = document.createElement("img");
@@ -19,23 +19,22 @@ imagem.setAttribute("width", "550");
 imagem.setAttribute("height", "396");
 imagem_fechar.setAttribute("src", "https://1.bp.blogspot.com/-4WAs-gBR-S4/V0YH-GiyJ3I/AAAAAAAADMA/7Lis8F2tDnc84nIudpUq79rOSz60_GfHQCLcB/s1600/fechar.png");
 imagem_fechar.setAttribute("id", "imagem_fechar");
-top = (clientHeight / 2) - (imagem.height / 2)
-left = (clientWidth / 2) - (imagem.width / 2)
-popup.setAttribute("style", "top: " + top + "px; left: " + left + "px");
+/*
+top = (clientHeight / 2);
+left = (clientWidth / 2);
+popup.setAttribute("style", "top: " + top + "px; left: " + left + "px");*/
 
 var mostrar_popup = function() {
-	if (visiblidade_popup) {
-		$(popup).fadeIn("fast", function() {
-			$(imagem).fadeIn("fast");
-		});
-	}
+	$(popup).fadeIn("fast", function() {
+		$(imagem).fadeIn("fast");
+	});
 }
 var verifica_se_mostra = function() {
 	scrollTop = $(window).scrollTop();
 	//if (scrollTop > 1482 && visiblidade_popup) {
-	if (scrollTop > 10) {
+	if (scrollTop > 10 && visibilidade_popup) {
 		mostrar_popup();
-		visiblidade_popup = true;
+		visibilidade_popup = false;
 	}
 }
 if (document.body.addEventListener) {
