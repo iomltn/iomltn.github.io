b8ed42356fb632182a4a691917a895c61fa5bb41
iomltn <iomltn@gmail.com>
2016-05-26 11:29:06 -0300

    corrigir popup

--- js/popup.js
+++ js/popup.js
@@ -31,7 +31,7 @@ var mostrar_popup = function() {
 }
 var verifica_se_mostra = function() {
 	scrollTop = $(window).scrollTop();
-	if (scrollTop > 1482 && visiblidade_popup) {
+	if (scrollTop > 1482 && visibilidade_popup) {
 	//if (scrollTop > 10 && visibilidade_popup) {
 		mostrar_popup();
 		visibilidade_popup = false;
