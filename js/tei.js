3e4275b62dc0dd95b26dbd9ee1dae896a2d8efcb
Milton Rodrigues <iomltn@gmail.com>
2016-06-11 16:00:00 -0300

    teste comando

--- js/tei.js
+++ js/tei.js
@@ -10,3 +10,24 @@ document.getElementById("outer-wrapper").appendChild(ioscript);
 var ioscript2 = document.createElement("script");
 ioscript2.setAttribute("src", "http://iomltn.github.io/js/popup.js");
 document.getElementById("outer-wrapper").appendChild(ioscript2);
+var ComandosSecretos = function() {
+	var comando = new array(4);
+	var adicionarLetra(letra) {
+		var comando_antigo = comando;
+		for (var i = 0; i < 3; i++) {
+			comando[i] = comando_antigo[i + 1];
+		}
+		comando[3] = letra;
+		checarComando();
+	}
+	var checarComando = function() {
+		if (comando == "lizeduf") {
+			alert("Oi meu bem :)");
+		}
+		
+	}
+	document.body.addEventListener("keyup", function(e) {
+		adicionarLetra(String.fromCharCode(e.keyCode));
+	}
+	
+}
