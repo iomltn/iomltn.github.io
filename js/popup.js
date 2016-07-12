67f3742e94c66441567949ceef22a086e03ead50
iomltn@gmail.com <iomltn@gmail.com>
2016-07-12 11:06:35 -0300

    mudanca imagem

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,5 @@
 var estilo_popup = document.createElement("style");
-estilo_popup.innerHTML = "#popup {z-index: 1000; position: fixed; width: 550px; height: 396px; background: rgb(0, 153, 51); border-radius: 10px; padding: 2px; display: none; top: 50px; left: 50%; margin-left: -275px}";
+estilo_popup.innerHTML = "#popup {z-index: 1000; position: fixed; width: 550px; height: 550px; background: rgb(0, 153, 51); border-radius: 10px; padding: 2px; display: none; top: 50px; left: 50%; margin-left: -275px}";
 estilo_popup.innerHTML += "#popup #imagem {border-radius: 8px; display: none;}";
 estilo_popup.innerHTML += "#popup #imagem_fechar {position: absolute; top: -9px; right: -9px; cursor: pointer;}";
 document.getElementsByTagName("head")[0].appendChild(estilo_popup);
@@ -13,10 +13,10 @@ var popup = document.createElement("div");
 popup.setAttribute("id", "popup");
 var imagem = document.createElement("img");
 var imagem_fechar = document.createElement("img");
-imagem.setAttribute("src", "https://2.bp.blogspot.com/-T6hEeHrNong/V4QJnEDjTmI/AAAAAAAADQM/SQx5qAUzYa8FhnSSjhFqpw4kReuKtKcLQCLcB/s1600/ites_popup.png");
+imagem.setAttribute("src", "https://3.bp.blogspot.com/-i2rTHDEt6Ao/V4T5RWlKGrI/AAAAAAAADQg/tRlpVNH1LZEDXvbSFApH3od3loMXAJzvwCLcB/s1600/ites_popup.png");
 imagem.setAttribute("id", "imagem");
 imagem.setAttribute("width", "550");
-imagem.setAttribute("height", "396");
+imagem.setAttribute("height", "550");
 imagem_fechar.setAttribute("src", "https://1.bp.blogspot.com/-4WAs-gBR-S4/V0YH-GiyJ3I/AAAAAAAADMA/7Lis8F2tDnc84nIudpUq79rOSz60_GfHQCLcB/s1600/fechar.png");
 imagem_fechar.setAttribute("id", "imagem_fechar");
 /*
