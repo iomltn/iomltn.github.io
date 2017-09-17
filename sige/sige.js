ed7aafe25015c111395f78301a505525f5eec22d
iomltn <iomltn@gmail.com>
2017-09-17 13:48:53 -0300

    sige

--- sige/sige.js
+++ sige/sige.js
@@ -53,8 +53,8 @@ for (var i = 3; i < trs.length; i++) {
 	}
 }
 
-function getNota(idAluno, idDisciplina, idAluno, idDisciplina) {
-	var str = "nr_Nota_" + alunos[i][0] + "_" + disciplinas[j][0];
+function getNota(idAluno, idDisciplina) {
+	var str = "nr_Nota_" + idAluno + "_" + idDisciplina;
 	var nota = document.getElementById(str);
 	return nota.value;
 }
@@ -75,7 +75,7 @@ function chamar() {
 		pp.document.write("<td>" + alunos[i][0] + "</td>");
 		pp.document.write("<td style=\"text-align: left\">" + alunos[i][1] + "</td>");
 		for (var j = 0; j < disciplinas.length; j++) {
-			var nota = getNota(alunos[i][0], disciplinas[j][0], alunos[i][0], disciplinas[j][0]);
+			var nota = getNota(alunos[i][0], disciplinas[j][0]);
 			pp.document.write("<td>" + nota + "</td>");
 		}
 		pp.document.write("</tr>");
